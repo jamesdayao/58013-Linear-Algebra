e3385e0f07e4e0424ccf5c7c9841820205dfda44
James Dayao <123426440+jamesdayao@users.noreply.github.com>
2023-11-06 07:45:20 +0800

    Add files via upload

---
+++ Midterm.m
@@ -0,0 +1,19 @@
+%Question 1
+A = [0,1; 1,0];
+
+B = 2;
+C = A+B;
+C
+
+%Question 2
+syms x y z;
+
+eq1 = 3*x - 5*y + 4*z == 5
+eq2 = 5*x + 2*y + z == 0
+eq3 = 2*x + 3*y - 2*z == 3
+
+sol = solve([eq1, eq2, eq3], [x, y, z]);
+
+xsol = sol.x
+ysol = sol.y
+zsol = sol.z
